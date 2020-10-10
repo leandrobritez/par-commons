8c03c9c1c7e6a3ce57d4cacb08dcffa042a5490b
leandrobritez <leandrobritez26@gmail.com>
2020-10-10 14:49:02 -0300

    version 1

---
+++ src/main/java/py/una/pol/par/commons/entity/BaseEntity.java
@@ -0,0 +1,31 @@
+package py.una.pol.par.commons.entity;
+
+/**
+ *
+ * @author Sourabh Sharma
+ * @param <T>
+ */
+public abstract class BaseEntity<T> extends Entity<T> {
+
+    private boolean isModified;
+
+    /**
+     *
+     * @param id
+     * @param name
+     */
+    public BaseEntity(T id, String name) {
+        super.id = id;
+        super.name = name;
+        isModified = false;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean isIsModified() {
+        return isModified;
+    }
+
+}
